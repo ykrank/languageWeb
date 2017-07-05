434765ad1393c78df279ef5305a694a11cf6319c
zhangyongfeng1 <503977995@qq.com>
2017-07-05 17:33:04 +0800

    添加安卓测试项目；lua加解密方法；go pdf文件
    添加安卓测试项目；lua加解密方法；go pdf文件

---
+++ code/android/resources/view/TextView/app/src/main/java/zhangyongfeng/textview/TextView.java
@@ -0,0 +1,13 @@
+package zhangyongfeng.textview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view);
+    }
+}
