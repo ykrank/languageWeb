513370b525bffef3022f7a3d9b8d4a35cecb0d48
zhangyongfeng1 <503977995@qq.com>
2017-07-07 17:04:39 +0800

    新建android 按钮测试项目
    新建android 按钮测试项目 Button_Text

---
+++ code/android/resources/view/Buttom_Text/app/src/main/java/zhangyongfeng/buttom_text/Button_Text.java
@@ -0,0 +1,13 @@
+package zhangyongfeng.buttom_text;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Button_Text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button__text);
+    }
+}
